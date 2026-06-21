e224e4e67d94296e69c078cb0b0bb1855b3d8ac9
Seth Kelvin <sethkelvina@gmail.com>
2026-06-21 14:34:14 +0000

    resolved status code assertion back to 200 to fix build

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,5 +8,5 @@ def client():
 
 def test_home_endpoint(client):
 	response = client.get('/')
-	assert response.status_code == 500
+	assert response.status_code == 200
 	assert b"CI/CD Lab Running Successfully" in response.data
